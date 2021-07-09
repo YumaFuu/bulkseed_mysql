0855ee40c7d6e5215b7bcf37399b69370562a396
YumaFuu <yuma.fuu05@gmail.com>
2021-07-09 22:00:57 +0900

    [add] sample

--- lib/bulkseed_mysql.rb
+++ lib/bulkseed_mysql.rb
@@ -1,8 +1,111 @@
 # frozen_string_literal: true
+require "pry"
+require "json"
+require 'terminal-table'
 
-require_relative "bulkseed_mysql/version"
+class BulkseedMysql
+  class Seed
+    def inspect
+      pretty_table = Terminal::Table.new do |t|
+        t << @columns
+        t.add_separator
 
-module BulkseedMysql
-  class Error < StandardError; end
-  # Your code goes here...
+        @values.each do |v|
+          t << v
+        end
+      end
+
+      <<~TXT
+        ======= #{@table} =========
+        #{ pretty_table }
+      TXT
+    end
+
+    def initialize(table)
+      @table = table
+      @values = []
+    end
+
+    def data= data
+      data.each do |item|
+        case item
+        when Array
+          @values << item
+        when Hash
+          @columns ||= item.keys
+
+          if @columns != item.keys
+            raise <<~TXT
+              Keys are not same to before
+              table: #{@table}
+              row: #{JSON.pretty_generate(item)}
+            TXT
+          end
+
+          @values << item.values
+        end
+      end
+    end
+
+    def columns= columns
+      @columns = columns
+    end
+
+    def table= table
+      @table = table
+    end
+
+    def to_cmd
+      values = @values.map do |rows|
+        vals = rows.map do |v|
+          # escape quotes
+          s = %Q(#{v}).gsub('"', '\"')
+          v.nil? ? "NULL" : "\"#{s}\""
+        end
+
+        "(#{ vals * "," })"
+      end
+
+      <<~SQL
+        REPLACE INTO #{@table}
+        (#{ @columns.map{ |a| %(`#{a}`) } * ',' })
+        VALUES #{ values * "," }
+      SQL
+    end
+  end
+
+  def self.call(name = nil, conn = nil, &block)
+    seed = new conn
+    seed.prepare name, &block
+    seed.call
+  end
+
+  attr_reader :seeds
+  attr_accessor :conn
+
+  def initialize(conn = nil)
+    @conn = conn
+    @conn ||= Mysql2::Client.new(
+      host: ENV["DB_HOST"],
+      username: ENV["DB_USER"],
+      password: ENV["DB_PASSWORD"],
+      database: "bulkseed_mysql_sample"
+    )
+
+    @seeds = []
+  end
+
+  def prepare(name = nil, &block)
+    seed = Seed.new name
+    yield seed
+
+    @seeds << seed
+  end
+
+  def call
+    @seeds.each do |s|
+      puts s.inspect
+      @conn.query s.to_cmd
+    end
+  end
 end
---
+++ sample/activerecord.rb
@@ -0,0 +1,108 @@
+require "./lib/bulkseed_mysql.rb"
+require "bundler/inline"
+
+gemfile do
+  gem "activerecord"
+  gem "mysql2"
+end
+
+require "active_record"
+
+db = "bulkseed_mysql_sample"
+ActiveRecord::Base.establish_connection(
+  adapter:  "mysql2",
+  host:     ENV["DB_HOST"],
+  username: ENV["DB_USER"],
+  password: ENV["DB_PASSWORD"],
+)
+
+con = ActiveRecord::Base.connection
+con.execute "DROP DATABASE IF EXISTS #{db}"
+con.execute "CREATE DATABASE IF NOT EXISTS #{db}"
+
+ActiveRecord::Base.establish_connection(
+  adapter:  "mysql2",
+  host:     ENV["DB_HOST"],
+  username: ENV["DB_USER"],
+  password: ENV["DB_PASSWORD"],
+  database: db,
+)
+
+class CreateSample < ActiveRecord::Migration[4.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :age
+      t.integer :sex
+      t.timestamps
+    end
+
+    create_table :posts do |t|
+      t.string :title
+      t.string :body
+      t.integer :user_id
+      t.index :user_id
+      t.timestamps
+    end
+  end
+end
+
+CreateSample.migrate :up
+
+class User < ActiveRecord::Base
+  enum sex: [:male, :female, :other]
+end
+
+class Post < ActiveRecord::Base
+end
+
+# -----------------------------------
+# Use prepare
+
+seed = BulkseedMysql.new
+seed.conn = ActiveRecord::Base.connection
+
+now = Time.now.to_s :db
+seed.prepare do |s|
+  s.table = User.table_name
+  s.columns = User.column_names
+  s.data = [
+    [1, "aaa", 23, User.sexes[:male], now, now],
+    [2, "bbb", 18, User.sexes[:female], now, now],
+    [3, "ccc", 18, User.sexes[:other], now, now],
+  ]
+end
+
+seed.call
+
+# -----------------------------------
+# Without prepare
+
+BulkseedMysql.call "posts" do |s|
+  s.data = [
+    {
+      id: 1,
+      user_id: 1,
+      title: "user1-1",
+      body: "body1",
+    },
+    {
+      id: 2,
+      user_id: 1,
+      title: "user1-2",
+      body: "body2",
+    },
+    {
+      id: 3,
+      user_id: 2,
+      title: "user2-1",
+      body: "body3",
+    },
+    {
+      id: 4,
+      user_id: 3,
+      title: "user3-1",
+      body: "body4",
+    },
+  ]
+end
