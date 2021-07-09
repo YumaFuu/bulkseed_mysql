57fbffd3d96dbd58d9e82b49f04e98c2fb49a9c3
Yuma Ishikawa <yuma.fuu05@gmail.com>
2021-07-09 18:15:55 +0900

    initial

---
+++ lib/bulkseed_mysql.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bulkseed_mysql/version"
+
+module BulkseedMysql
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "bulkseed_mysql"
+
+require "test-unit"
